7098d94d974609b30755408b5e9746e97b343bac
axel <ogdabou@gmail.com>
2013-05-11 14:26:46 +0200

    added convertTogeyScale

---
+++ PluginSource/src/plugin/basic/ConvertToGreyScale.java
@@ -0,0 +1,30 @@
+package plugin.basic;
+
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+
+import plugin.IPlugin;
+
+public class ConvertToGreyScale implements IPlugin{
+
+	@Override
+	public BufferedImage perform(BufferedImage img) {
+		BufferedImage result = new BufferedImage(img.getWidth(),
+				img.getHeight(), BufferedImage.TYPE_BYTE_GRAY);
+		
+		Graphics painter = result.getGraphics();
+		painter.drawImage(img, 0, 0, null);
+		painter.dispose();
+		
+		Graphics2D painter2D = (Graphics2D)painter;
+		painter2D.drawImage(result, null, 0, 0);
+		return result;
+	}
+
+	@Override
+	public String getName() {
+		return "GreyScale conversion";
+	}
+
+}
